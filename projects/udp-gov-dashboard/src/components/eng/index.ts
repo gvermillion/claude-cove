c3727dc771ae7e05c80f9437de1dd3266e94cc13
Grant Vermillion <git@grant.vermillion.pro>
2026-04-28 22:47:42 -0700

    refactor(udp-dashboard): add eng/index.ts barrel

---
+++ projects/udp-gov-dashboard/src/components/eng/index.ts
@@ -0,0 +1,9 @@
+// src/components/eng/index.ts
+export { default as OverviewView } from './OverviewView';
+export { default as SandboxView } from './SandboxView';
+export { default as EnforcementView } from './EnforcementView';
+export { default as TaxonomyView } from './TaxonomyView';
+export { default as SchemaView } from './SchemaView';
+export { default as PolicyView } from './PolicyView';
+export { default as ExtensibilityView } from './ExtensibilityView';
+export { default as OpsView } from './OpsView';
